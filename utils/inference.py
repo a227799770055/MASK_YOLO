05b59e08aa7181be835943dbb026c27e4e58c2dd
Eddie Hsiao <a227799770055@gmail.com>
2022-09-14 11:42:33 +0800

    [Feat] add fitEllipse to replace morph

--- utils/inference.py
+++ utils/inference.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
-from genericpath import isdir
-import sys, os
+import sys, os, time
 sys.path.append('/home/insign/Doc/insign/Mask_yolo')
 import torch
 import cv2
@@ -12,8 +11,6 @@ from model.head_RCNN import *
 from model.groundtrue_import import *
 from PIL import Image
 from torchvision import transforms
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
 
 def image_loading(img_path):
     image = cv2.imread(str(img_path))
@@ -28,14 +25,15 @@ def image_loading(img_path):
     
     return image, im0s, img_h, img_w
 
-def merge_mask_image(mask, im0s, name):
+def merge_mask_image(mask, im0s, name, retval):
     mask_h, mask_w = mask_logits.shape[0], mask_logits.shape[1]
     im0s_roi = im0s[int(boxes[0][1]):int(boxes[0][1]+mask_h), int(boxes[0][0]):int(boxes[0][0]+mask_w)]
-    det = cv2.addWeighted(im0s_roi,0.7 ,mask_logits, 0.3, 0)
+    retval_bg = np.zeros((im0s_roi.shape[0], im0s_roi.shape[1],3), np.uint8)
+    retval_bg = cv2.ellipse(retval_bg, retval, (0, 0, 255), thickness=-1)
+    # det = cv2.addWeighted(im0s_roi,0.7 ,mask_logits, 0.3, 0)
+    det = cv2.addWeighted(im0s_roi,0.7 ,retval_bg, 0.3, 0)
     im0s[int(boxes[0][1]):int(boxes[0][1]+mask_h), int(boxes[0][0]):int(boxes[0][0]+mask_w)] = det
     return im0s
-    
-
 
 def model_detection(image, yolo, mask_head, cfg):
     #   Predict bounding box
@@ -60,13 +58,18 @@ def model_detection(image, yolo, mask_head, cfg):
     mask_logits.dtype = 'uint8'
     mask = deepcopy(mask_logits)
     mask_logits = mask_logits*255
-    kernel = np.ones((5,5),np.uint8)
-    mask_logits =  cv2.erode(mask_logits,kernel,iterations = 3)
-    mask_logits =  cv2.dilate(mask_logits,kernel,iterations = 5)
-    # mask_logits = cv2.morphologyEx(mask_logits, cv2.MORPH_OPEN, kernel) #morphology layer 
-    # mask_logits = cv2.GaussianBlur(mask_logits, (0,0), sigmaX=5, sigmaY=5, borderType = cv2.BORDER_DEFAULT)
+    #   morphology
+    # kernel = np.ones((5,5),np.uint8)
+    # mask_logits =  cv2.erode(mask_logits,kernel,iterations = 3)
+    # mask_logits =  cv2.dilate(mask_logits,kernel,iterations = 5)
+    s1= time.time()
     mask_logits = cv2.cvtColor(mask_logits, cv2.COLOR_GRAY2RGB)
-    return boxes, mask_logits, mask
+
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    retval = cv2.fitEllipse(contours[0])
+    e1= time.time()
+    print( 'fitEllipse',(e1-s1)*1000, 'ms')
+    return boxes, mask_logits, retval
 
     
 if __name__ == '__main__':
@@ -82,28 +85,35 @@ if __name__ == '__main__':
     if not os.path.isdir(save_dir):
         os.mkdir(save_dir)
 
-    device = 'cpu'
-    
+    device = 'cuda'
+
+    #   Model yolo_backbone loading
+    yolo = model_manipulate(cfg['model']['weight']).eval().to(device)
+    #   Model roi_head loading
+    mask_head = torch.load(roimodelPath).eval().to(device)
+
     imgs = os.listdir(imgDir)
     for i in imgs:
         imgPath = os.path.join(imgDir, i)
         name = i.split('.')[0]
-
+        
+        s = time.time()
         #   Image loading
         image, im0s, img_h, img_w = image_loading(imgPath)
 
-        #   Model yolo_backbone loading
-        yolo = model_manipulate(cfg['model']['weight']).eval().to(device)
-        #   Model roi_head loading
-        mask_head = torch.load(roimodelPath).eval().to(device)
-
         #   prediction
-        boxes, mask_logits, mask = model_detection(image, yolo, mask_head, cfg)
-
+        
+        s2 = time.time()
+        boxes, mask_logits, retval = model_detection(image, yolo, mask_head, cfg)
+        e2 = time.time()
+        print('Mask Predict', (e2-s2)*1000, 'ms')
+        
         if len(boxes) != 0:
             #   Merge mask and image
-            im0s = merge_mask_image(mask=mask_logits, im0s=im0s, name=name)
+            im0s = merge_mask_image(mask=mask_logits, im0s=im0s, name=name, retval=retval)
             cv2.imwrite('{}/{}_det.jpg'.format(save_dir, name),im0s)
         else:
             print("-"*15)
             print("Do not detect polyp")
+        e = time.time()
+        print('Total Time Duration = {} ms'.format((e-s)*1000))
